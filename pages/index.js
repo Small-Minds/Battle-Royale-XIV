2b6e72f87b01ace9d227799216084a9b76e3913c
Ryan Fleck <ryan.fleck@protonmail.com>
2020-12-08 16:47:09 -0700

    Change cards.

--- pages/index.js
+++ pages/index.js
@@ -29,31 +29,19 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
+          <a href="" className="card">
+            <h3>Sponsor Us &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Get your technology or gaming brand featured in our event and
+              event advertising.
+            </p>
+          </a>
+
+          <a href="" className="card">
+            <h3>Register &rarr;</h3>
+            <p>
+              Sign up for a chance to win cash prizes. Most of your ticket will
+              be donated to charity.
             </p>
           </a>
         </div>
